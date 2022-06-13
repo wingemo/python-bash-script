2c5df33c8fc0c783fbb234fbf4ce135bc24b814d
Wingemo <philip.wingemo@outlook.com>
2022-06-14 01:53:21 +0200

    Update

--- weather.py
+++ weather.py
@@ -1,12 +1,21 @@
 #import the necessary package!
 import requests
+import json
+
+WMO = {
+  0: "Clear sky",
+  1: "Mainly clear",
+  2: "partly cloudy"
+}
 
 #input the city name
 city = 'Stockholm'
 
 #fetch the weater details
-url = 'https://wttr.in/{}'.format(city)
+url = 'https://api.open-meteo.com/v1/forecast?latitude=59.36&longitude=18.26&current_weather=true&timezone=Europe%2FBerlin'
 res = requests.get(url)
-
+b = json.loads(res.text)
 #display the result!
-print(res.text)
+
+print(WMO[b['current_weather']['weathercode']])
+print(b['current_weather']['temperature'])
